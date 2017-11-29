37e51e558bc1d27bb67db64055db313cd87772cb
farihakhan <fariha83@hotmail.com>
2017-11-28 23:15:58 -0800

    Trying to fix quadratic

--- R/quadrtc.R
+++ R/quadrtc.R
@@ -4,10 +4,9 @@
 #' Where x represents an unknown
 #' While a, b, and c are constants with a not equal to 0
 #'
-#' @param a
-#' @param b
-#' @param c
-#' @param genplot Generates and prints a plot of \code{x}
+#' @param a of \code{expression(ax^2 + bx + c )}
+#' @param b of \code{expression(ax^2 + bx + c )}
+#' @param c of \code{expression(ax^2 + bx + c )}
 #' \code{FALSE} by default.
 #'
 #'
@@ -18,7 +17,7 @@
 #' here \code{\link{https://en.wikipedia.org/wiki/Quadratic_formula}}
 #'
 #' @return
-#' The value(s) of x based on \code{x}
+#' The value(s) of x based on \code{a,b,c}
 #'
 #'
 #' @examples
@@ -28,26 +27,33 @@
 #'
 #' @rdname quadrtc
 #' @export
-quadrtc <- function(a,b,c, genplot=FALSE) {
+quadrtc <- function(a,b,c, qdplot=FALSE) {
 
       #' D > 0
       if(delta(a,b,c) > 0){
             x1 = (-b + sqrt(delta(a,b,c))) / (2*a)
             x2 = (-b - sqrt(delta(a,b,c))) / (2*a)
             result = c(x1, x2)
+            print(paste("Two real roots: ", x1, x2))
+
       }
 
       #' D == 0
       else if(delta(a,b,c) == 0){
             x = -b/(2*a)
+            ax2 + bx + c
+            print(paste("One real root: ", x))
       }
 
       #' D < 0
       else {"No real roots"}
 }
 
+
 #' Define internal function for delta
 #' Rearrange expression(ax^2 + bx + c) to delta expression(delta = b^2 - 4ac)
 delta<-function(a,b,c){
       (b^2)-(4*a*c)
 }
+
+
